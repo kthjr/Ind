91be82afc424594e01ea950b97ea016eb8fb7dbf
Valeria Bryzgalova <bryzgalovalera@mail.ru>
2018-12-04 13:16:40 +0300

    Initial commit

---
+++ project_sam/project_sam.cpp
@@ -0,0 +1,13 @@
+// project_sam.cpp: определяет точку входа для консольного приложения.
+
+ #include "stdafx.h"
+ #include <iostream>
+ using namespace std;
+
+int main()
+{
+	system("pause");
+
+	return 0;
+}
+
