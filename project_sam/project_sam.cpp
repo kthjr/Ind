d41b1a7701fe3bccda37faf973fffb3285405004
Valeria Bryzgalova <bryzgalovalera@mail.ru>
2018-12-04 13:40:27 +0300

    Added exit function

--- project_sam/project_sam.cpp
+++ project_sam/project_sam.cpp
@@ -8,44 +8,51 @@ int main()
 {
 
 	int a, b, c, input;
-	
-	cout<<"Vvedite pervoe chislo:";
-	cin>>a;
-	cout<<"Vvedite vtoroe chislo:";
-	cin>>b;
 
 	printf( "Vyberite deistvie?\n" );
     printf( "1. +\n" );
     printf( "2. -\n" );
     printf( "3. *\n" );
     printf( "4. /\n" );
+	printf( "5. Vyhod\n" );
+
 
 	printf( "Vash vybor: " );
     scanf("%d", &input );
 
-	switch ( input ) {
-        case 1:           
-            c = a+b;
-			cout<<"Otvet:"<<c<<endl;
-            break;
-        case 2:
-            c = a-b;
-			cout<<"Otvet:"<<c<<endl;
-            break;
-        case 3:
-            c = a*b;
-			cout<<"Otvet:"<<c<<endl;
-            break;
-        case 4:
-			if (b != 0) 
-			{
-				c = a/b;
+	if (input != 5) {
+
+		cout<<"Vvedite pervoe chislo:";
+		cin>>a;
+		cout<<"Vvedite vtoroe chislo:";
+		cin>>b;
+
+		switch ( input ) {
+			case 1:           
+				c = a+b;
 				cout<<"Otvet:"<<c<<endl;
-			}
-			else 
-			{
-				printf( "Division by zero.\n" );
-			}
+				break;
+			case 2:
+				c = a-b;
+				cout<<"Otvet:"<<c<<endl;
+				break;
+			case 3:
+				c = a*b;
+				cout<<"Otvet:"<<c<<endl;
+				break;
+			case 4:
+				if (b != 0) 
+				{
+					c = a/b;
+					cout<<"Otvet:"<<c<<endl;
+				}
+				else 
+				{
+					printf( "Division by zero.\n" );
+				}
+
+		}
+
 	}
 
 	system("pause");
